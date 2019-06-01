f746fc70ac54ff8ea1b4704795a62724b40f02dc
wyleu <wyleus@gmail.com>
2019-06-01 13:48:52 +0100

    First Base Objects

--- display/models.py
+++ display/models.py
@@ -1,14 +1,22 @@
-import uuid
 from django.db import models
 from thing.models import Base
 
 SHAPES_CHOICE = (
-    'rect','Rectangle',
-    'circle', 'Circle',
-    'ellipse', 'Ellipse',
-    'line', 'Line',
-    'polyline','Polylone',
-    'polygon','Polygon'
+    ('rect', 'Rectangle', ),
+    ('circle', 'Circle', ),
+    ('ellipse', 'Ellipse', ),
+    ('line', 'Line',),
+    ('polyline', 'Polylone', ),
+    ('polygon', 'Polygon', ),
+)
+
+INITIAL_COLOUR_CHOICE = (
+    ('red', 'Red', ),
+    ('green', 'Green', ),
+    ('blue', 'Blue', ),
+    ('white', 'White',),
+    ('black', 'Black', ),
+    ('grey', 'Grey', ),
 )
 
 
@@ -21,8 +29,15 @@ class DisplayBase(Base):
 
 
 class Colour(DisplayBase):
+    name = models.CharField(
+        max_length=8,
+        choices=INITIAL_COLOUR_CHOICE,
+        unique=True,
+        default='red',
+        help_text="""The DisplayBase name field"""
+    )
     r = models.IntegerField(
-        default=0,
+        default=128,
         help_text='Red Value'
     )
     g = models.IntegerField(
@@ -34,17 +49,27 @@ class Colour(DisplayBase):
         help_text='Blue Value'
     )
     a = models.IntegerField(
-        default=0,
+        default=255,
         help_text='Alpha Value'
     )
+
+
+def get_default_colour():
+    return Shape.objects.get_or_create(name=INITIAL_COLOUR_CHOICE[0][0])[0]
 
 
 class Shape(DisplayBase):
     name = models.CharField(
         max_length=8,
         choices=SHAPES_CHOICE,
+        unique=True,
+        default='circle',
         help_text="""The DisplayBase name field"""
     )
+
+
+def get_default_shape():
+    return Shape.objects.get_or_create(name=SHAPES_CHOICE[0][0])[0]
 
 
 class Position(DisplayBase):
@@ -62,7 +87,12 @@ class Position(DisplayBase):
     )
 
 
+def get_default_position():
+    return Shape.objects.get_or_create()[0]
+
+
 class Rotation(DisplayBase):
+
     x = models.FloatField(
         default=0,
         help_text='XRot Value'
@@ -80,7 +110,7 @@ class Rotation(DisplayBase):
 class Size(DisplayBase):
     size = models.FloatField(
         help_text="""The DisplayBase name field""",
-        default = 1.0
+        default=1.0
     )
 
 
@@ -88,29 +118,54 @@ class DisplayObject(DisplayBase):
     class Meta:
         abstract = True
 
-    shape = models.ForeignKey('Shape')
-    colour= models.ForeignKey('Colour')
-    position = models.ForeignKey('Position')
-    rotation = models.ForeignKey('Rotation')
-    size = models.ForeignKey('Size')
+    shape = models.ForeignKey(
+        'Shape',
+        to_field='name',
+        on_delete=models.SET(get_default_shape),
+    )
+    colour = models.ForeignKey(
+        'Colour',
+        on_delete=models.SET(get_default_colour),
+        to_field='name'
+    )
+    # position = models.ForeignKey(
+    #     'Position',
+    #     on_delete=models.SET(get_default_position),
+    #
+    # )
+    # rotation = models.ForeignKey('Rotation')
+    # size = models.ForeignKey('Size')
 
 
 class Node(DisplayObject):
-    pass
-
-
+    x = models.FloatField(
+        default=0,
+        help_text='XPos Value'
+    )
+    y = models.FloatField(
+        default=0,
+        help_text='YPos Value'
+    )
+    z = models.FloatField(
+        default=0,
+        help_text='Zpos Value'
+    )
+#
+#
 class Link(DisplayObject):
     node1 = models.ForeignKey(
         'Node',
-        related_name='LinkNode1'
+        related_name='LinkNode1',
+        on_delete=models.CASCADE
     )
     node2 = models.ForeignKey(
         'Node',
-        related_name='LinkNode2'
+        related_name='LinkNode2',
+        on_delete=models.CASCADE
     )
-
-
-
-
-class BackGround(DisplayBase):
-    colour= models.ForeignKey('Colour')
+#
+#
+#
+#
+# class BackGround(DisplayBase):
+#     colour= models.ForeignKey('Colour')
---
+++ thing/models.py
@@ -0,0 +1,44 @@
+import uuid
+from django.db import models
+
+
+class Base(models.Model):
+    """
+    Base Model  Class with UUID identification
+    """
+    class Meta:
+        abstract = True
+        ordering = [
+            'order',
+            '-modified_date',
+            'name',
+        ]
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4, editable=False,
+        help_text="""Base ID UUID field"""
+    )
+    name = models.CharField(
+        max_length=64,
+        blank=True,
+        help_text="""The base name field"""
+    )
+    desc = models.TextField(
+        blank=True,
+        help_text="""The base desc field"""
+    )
+    attrib = models.TextField(
+        blank=True,
+        help_text="""The base attrib field"""
+    )
+    order = models.IntegerField(
+        default=0,
+        help_text="""Base Order Field"""
+    )
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+    modified = models.DateTimeField(
+        auto_now=True
+    )
