dae8924a257a2b3f596236b661213f5855a0adec
wyleu <wyleus@gmail.com>
2019-06-03 19:59:43 +0100

    Adding static file settings

--- thing/urls.py
+++ thing/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from display.views import BaseView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('display/', BaseView.as_view()),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
